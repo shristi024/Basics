9fb7df555291ff82781e684f0b86202092197e03
Shristi024 <shristi060624@gmail.com>
2026-05-26 00:13:03 +0530

    integer input

--- L1striver.cpp
+++ L1striver.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello Everyone!";
-    std::cout << "Hello Everyone!";
+    int x,y;
+    cout << "Hello Everyone!" << "\n" << "I am learning c++" << "\n" <<"I am also learning dsa" <<"\n";
+    cout << "I will learn dsa in pseudo code with striver" << "\n" << "Then i will do dsa in python,c and c++" << "\n";
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y;
     return 0;
 }
